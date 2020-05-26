c8226ec85ec8f3a5530e4273b922ab881d24ad73
fbecerra <becerrafernando@gmail.com>
2020-05-25 20:16:15 -0400

    agrega datos de fallecidos

--- js/main.js
+++ js/main.js
@@ -14,6 +14,18 @@ var margin = {top: 50, right: 80, bottom: 50, left: 50},
 
 var dateParse = d3.timeParse("%Y-%m-%d");
 
+var state = {
+      indicador: null,
+      cantidad: null,
+      unidad: null,
+      macrozona: null,
+      microzona: null,
+      escala: null,
+      data: null,
+      dates: null,
+      yLabel: null,
+    }
+
 var xScale = d3.scaleTime()
     .range([margin.left, width - margin.right])
 var yScale;
@@ -23,8 +35,6 @@ var yLogScale = d3.scaleLog()
     .range([height - margin.bottom, margin.top])
 var line = d3.line()
     .curve(d3.curveMonotoneX);
-var line2 = d3.line()
-    .curve(d3.curveMonotoneX)
 
 var svg = plot.append("svg")
     .attr("width", width + margin.left + margin.right)
@@ -46,218 +56,298 @@ var xAxis = d3.axisBottom()
             .tickSizeOuter(0);
 var yAxis = d3.axisLeft();
 
+var curves = g.append("g")
+    .attr("fill", "none")
+    .attr("stroke-width", 1.5)
+    .attr("stroke-linejoin", "round")
+    .attr("stroke-linecap", "round")
+    .selectAll("path")
+
 var nameNoSpaces = function(name) {
   return name.toLowerCase().split(" ").join("");
 }
 
 Promise.all([
-    d3.csv('https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto1/Covid-19.csv')
+    d3.csv('https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto1/Covid-19.csv'),
+    d3.csv('https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto14/FallecidosCumulativo.csv')
 ]).then(function(data) {
 
-    // console.log(data);
-    datesString = data[0].columns.filter(d => d.slice(0,4) == '2020')
-    dates = datesString.map(d => dateParse(d))
-    data[0].forEach(function(ele){
-      ele.values = datesString.map(d => +ele[d])
-      // console.log(ele)
-    })
-    // console.log(dates);
+    // Add listeners to form objects
+    addListeners();
+    drawPlot();
 
-    xScale.domain(d3.extent(dates))
-    xAxis.scale(xScale)
-      .ticks(dates.length);
+    function addListeners() {
+        indicador = document.querySelector('#indicador');
+        cantidad = document.querySelector('#cantidad');
+        unidad = document.querySelector('#unidad');
+        macrozona = document.querySelector('#macrozona');
+        microzona = document.querySelector('#microzona');
+        escala = document.querySelector('#escala');
 
-    yScale = d3.scaleLinear()
-        .range([height - margin.bottom, margin.top])
-        .domain([0, d3.max(data[0], d => d3.max(d.values))]).nice()
-    yAxis.scale(yScale);
+        state.indicador = indicador.value;
+        state.cantidad = cantidad.value;
+        state.unidad = unidad.value;
+        state.macrozona = macrozona.value;
+        state.microzona = microzona.value;
+        state.escala = escala.value;
 
-    line.x(d => xScale(d.x))
-      .y(d => yScale(d.y))
-    line2.x((d, i) => xScale(dates[i]))
-      .y(d => yScale(d))
-
-    gXAxis.call(xAxis);
-    gYAxis.call(yAxis)
-      .call(g => g.select(".domain").remove())
-      .call(g => g.select(".tick:last-of-type text").clone()
-      .attr("x", 3)
-      .attr("text-anchor", "start")
-      .attr("font-weight", "bold")
-      .text("Casos Totales"));
-
-    console.log(data[0]);
-
-    var curves = g.append("g")
-      .attr("fill", "none")
-      .attr("stroke-width", 1.5)
-      .attr("stroke-linejoin", "round")
-      .attr("stroke-linecap", "round")
-    .selectAll("path")
-
-
-    curves.data(data[0]).enter().append("path")
-    // .join("path")
-      .style("mix-blend-mode", "multiply")
-      .attr("opacity", 0.8)
-      .attr("class", d => "curve "+nameNoSpaces(d.Comuna))
-      // .attr("stroke", "lightgray")
-      .attr("stroke", d => d3.interpolateViridis(d3.max(d.values, e => e)/yScale.domain()[1]))
-      .attr("d", d => line2(d.values))
-      // .on("mouseover", function(){
-      //   d3.selectAll(".curve")
-      //     .attr("opacity", 0.5)
-      //     .attr("stroke","lightgray")
-      //   d3.select(this).attr("opacity", 1.0)
-      //     .attr("stroke", d => d3.interpolateViridis(d3.max(d.values, e => e)/yScale.domain()[1]))
-      // })
-      // .on('mouseleave', function(){
-      //   d3.selectAll(".curve")
-      //     .attr("opacity", 0.8)
-      //     .attr("stroke", d => d3.interpolateViridis(d3.max(d.values, e => e)/yScale.domain()[1]))
-      // });
-
-    svg.call(hover, curves)
-
-    function hover(svg, path) {
-
-      if ("ontouchstart" in document) svg
-          .style("-webkit-tap-highlight-color", "transparent")
-          .on("touchmove", moved)
-          .on("touchstart", entered)
-          .on("touchend", left)
-      else svg
-          .on("mousemove", moved)
-          .on("mouseenter", entered)
-          .on("mouseleave", left);
-
-      const dot = svg.append("g")
-          .attr("display", "none");
-
-      dot.append("circle")
-          .attr("r", 2.5);
-
-      dot.append("text")
-          .attr("font-family", "sans-serif")
-          .attr("font-size", 10)
-          .attr("text-anchor", "middle")
-          .attr("y", -8);
-
-      function moved() {
-        d3.event.preventDefault();
-        const mouse = d3.mouse(this);
-        const xm = xScale.invert(mouse[0]-margin.left); // TODO: CONSTRAIN WITHIN RIGHT MARGIN
-        const ym = yScale.invert(mouse[1]-margin.top);
-        const i1 = d3.bisectLeft(dates, xm, 1);
-        const i0 = i1 - 1;
-        const i = xm - dates[i0] > dates[i1] - xm ? i1 : i0;
-        const s = d3.least(data[0], d => Math.abs(d.values[i] - ym));
-        // console.log(s)
-        d3.selectAll(".curve")
-            .attr("opacity", 0.5)
-            .attr("stroke","lightgray")
-        d3.select(".curve."+nameNoSpaces(s.Comuna))
-          .attr("opacity", 1.0)
-          .attr("stroke", d => d3.interpolateViridis(d3.max(s.values, e => e)/yScale.domain()[1]))
-        path.attr("stroke", d => d === s ? null : "#ddd").filter(d => d === s).raise();
-        dot.attr("fill", d => d3.interpolateViridis(d3.max(s.values, e => e)/yScale.domain()[1]))
-          .attr("transform", `translate(${xScale(dates[i])+margin.left},${yScale(s.values[i])+margin.top})`);
-        dot.select("text").text(s.values[i]); // TODO: IF LOG THEN WE NEED TO SUBSTRACT ONE
-      }
-
-      function entered() {
-        path.style("mix-blend-mode", null).attr("stroke", "#ddd");
-        dot.attr("display", null);
-      }
-
-      function left() {
-        d3.selectAll(".curve")
-            .attr("opacity", 0.8)
-            .attr("stroke", d => d3.interpolateViridis(d3.max(d.values, e => e)/yScale.domain()[1]))
-        path.style("mix-blend-mode", "multiply").attr("stroke", null);
-        dot.attr("display", "none");
-      }
-    }
-
-    // data[0].forEach(function(ele){
-    //
-    //
-    //   texts.attr("x", width - margin.left/2 - margin.right/2)
-    //     .attr("y", yScale(datos[datos.length-1].y) + 5)
-    //     .attr("fill", d3.interpolateViridis(datos[datos.length-1].y/yScale.domain()[1]))
-    //     .attr("class", "name "+nameNoSpaces(ele.Comuna))
-    //     // .attr("opacity", 0.5)
-    //     .attr("text-anchor", "start")
-    //     .attr("font-size", "1rem")
-    //     // .attr("font-weight", "bold")
-    //     .on("mouseover", function(){
-    //       d3.selectAll(".curve")
-    //         .attr("opacity", 0.2)
-    //       d3.select(".curve."+nameNoSpaces(ele.Comuna)).attr("opacity", 1.0)
-    //
-    //       d3.selectAll(".name")
-    //         .attr("opacity", 0.2)
-    //       d3.select(".name."+nameNoSpaces(ele.Comuna)).attr("opacity", 1.0)
-    //     })
-    //     .on('mouseleave', function(){
-    //       d3.selectAll(".curve")
-    //         .attr("opacity", 1.0)
-    //
-    //       d3.selectAll(".name")
-    //         .attr("opacity", 1.0)
-    //     });
-    // })
-
-    // Update yAxis scale
-    d3.select("#escala")
-      .on("change", function(d){
-        let formValue = this.value;
-        console.log(formValue)
-
-        if (formValue === "escala-logaritmica"){
-          yScale = d3.scaleLog()
-              .range([height - margin.bottom, margin.top])
-              .domain([1, d3.max(data[0], d => d3.max(d.values)) + 1])
-          yAxis.scale(yScale)
-              .tickValues([2, 5, 10, 20, 50, 100, 200, 500, 1000, 2000])
-              // .ticks(2)
-              .tickFormat(d3.format('i'))
-          line2.y(d => yScale(d + 1));
-        } else if (formValue === "escala-lineal") {
-          yScale = d3.scaleLinear()
-              .range([height - margin.bottom, margin.top])
-              .domain([0, d3.max(data[0], d => d3.max(d.values))]).nice()
-          yAxis.scale(yScale);
-          line2.y(d => yScale(d));
+        if (state.indicador == 'casos') {
+          state.data = data[0];
+          state.yLabel = 'Casos'
+        } else if (state.indicador == 'muertes') {
+          state.data = data[1];
+          state.yLabel = 'Muertes'
         }
 
-        d3.select("g.axis.y")
-          .transition(200)
-          .duration(500)
-          .call(yAxis);
+        // if (series_pk !== null) series_pk.addEventListener('change', changeSeries);
+        if (indicador !== null) indicador.addEventListener('change', function(){
+          state.indicador = indicador.value;
+          if (state.indicador == 'casos') {
+            state.data = data[0];
+            state.yLabel = 'Casos';
+            state.microzona = 'Comuna';
+          } else if (state.indicador == 'muertes') {
+            state.data = data[1];
+            state.yLabel = 'Muertes';
+            state.microzona = 'Region'; // TODO: Change value of "microzona" object and lock microzona
+          }
+          console.log(state)
+          drawPlot();
+        });
+        if (escala !== null) escala.addEventListener('change', function(){
+          state.escala = escala.value;
+          drawPlot();
+          // loadData();
+        });
+        // if (colorby !== null) colorby.addEventListener('change', function(){
+        //   state.fields_schema.colorby.selected = colorby.value;
+        //   drawPoints();
+        // });
+        // if (hover !== null) hover.addEventListener('change', function(){
+        //   state.fields_schema.hover.selected = hover.value;
+        // });
+        // if (coords !== null) coords.addEventListener('change', function(){
+        //   if (coords.value == 't-SNE') {
+        //     state.currentCoords.x = "tSNE_X.coord.coord_tSNE.amp_RA.info.csv.9";
+        //     state.currentCoords.y = "tSNE_Y.coord.coord_tSNE.amp_RA.info.csv.9";
+        //   } else if (coords.value == 'PCA') {
+        //     state.currentCoords.x = "PC1.coord.coord_PCA.0.1.n6.info.csv.10";
+        //     state.currentCoords.y = "PC2.coord.coord_PCA.0.1.n6.info.csv.10"
+        //   }
+        //   drawPoints();
+        // });
+    }
 
-        var selection = d3.selectAll(".curve").data(data[0])
+    function drawPlot() {
 
-        selection.enter()
-          .append("path")
-          .transition(500)
-          .attr("d", function(d){
-            return line2(d.values)
-          })
-
-        selection.transition(500)
-          .attr("d", function(d){
-            return line2(d.values)
-          })
-
-        selection.exit().remove()
-
-
-        // texts.transition()
-        //   .duration(500)
-        //   .attr("y", 0)
+      datesString = state.data.columns.filter(d => d.slice(0,4) == '2020')
+      dates = datesString.map(d => dateParse(d))
+      state.data.forEach(function(ele){
+        ele.values = datesString.map(d => +ele[d]);
+        ele.Poblacion = +ele.Poblacion;
       })
 
+      console.log(state, dates)
+
+      xScale.domain(d3.extent(dates))
+      xAxis.scale(xScale)
+        .ticks(dates.length);
+
+      yScale = d3.scaleLinear()
+          .range([height - margin.bottom, margin.top])
+          .domain([0, d3.max(state.data, d => d3.max(d.values))]).nice()
+      yAxis.scale(yScale);
+
+      line.x((d, i) => xScale(dates[i]))
+        .y(d => yScale(d))
+
+      gXAxis.call(xAxis);
+      gYAxis.call(yAxis)
+        .call(g => g.select(".domain").remove())
+        .call(g => g.select(".tick:last-of-type text").clone()
+        .attr("x", 3)
+        .attr("text-anchor", "start")
+        .attr("font-weight", "bold")
+        .text("Casos Totales"));
+
+      var path = g.selectAll("path").data(state.data)
+
+      path.attr("fill", "none")
+        .attr("stroke-width", 1.5)
+        .attr("stroke-linejoin", "round")
+        .attr("stroke-linecap", "round")
+        .style("mix-blend-mode", "multiply")
+        .attr("opacity", 0.8)
+        .attr("class", d => "curve "+nameNoSpaces(d[state.microzona]))
+        // .attr("stroke", "lightgray")
+        .attr("stroke", d => d3.interpolateViridis(d3.max(d.values, e => e)/yScale.domain()[1]))
+        .attr("d", d => line(d.values))
+
+      path.enter().append("path")
+      // .join("path")
+        .attr("fill", "none")
+        .attr("stroke-width", 1.5)
+        .attr("stroke-linejoin", "round")
+        .attr("stroke-linecap", "round")
+        .style("mix-blend-mode", "multiply")
+        .attr("opacity", 0.8)
+        .attr("class", d => "curve "+nameNoSpaces(d[state.microzona]))
+        // .attr("stroke", "lightgray")
+        .attr("stroke", d => d3.interpolateViridis(d3.max(d.values, e => e)/yScale.domain()[1]))
+        .attr("d", d => line(d.values))
+        // .on("mouseover", function(){
+        //   d3.selectAll(".curve")
+        //     .attr("opacity", 0.5)
+        //     .attr("stroke","lightgray")
+        //   d3.select(this).attr("opacity", 1.0)
+        //     .attr("stroke", d => d3.interpolateViridis(d3.max(d.values, e => e)/yScale.domain()[1]))
+        // })
+        // .on('mouseleave', function(){
+        //   d3.selectAll(".curve")
+        //     .attr("opacity", 0.8)
+        //     .attr("stroke", d => d3.interpolateViridis(d3.max(d.values, e => e)/yScale.domain()[1]))
+        // });
+
+      path.exit().remove()
+
+      svg.call(hover, curves)
+
+      function hover(svg, path) {
+
+        if ("ontouchstart" in document) svg
+            .style("-webkit-tap-highlight-color", "transparent")
+            .on("touchmove", moved)
+            .on("touchstart", entered)
+            .on("touchend", left)
+        else svg
+            .on("mousemove", moved)
+            .on("mouseenter", entered)
+            .on("mouseleave", left);
+
+        const dot = svg.append("g")
+            .attr("display", "none");
+
+        dot.append("circle")
+            .attr("r", 2.5);
+
+        dot.append("text")
+            .attr("font-family", "sans-serif")
+            .attr("font-size", 10)
+            .attr("text-anchor", "middle")
+            .attr("y", -8);
+
+        function moved() {
+          d3.event.preventDefault();
+          const mouse = d3.mouse(this);
+          const xm = xScale.invert(mouse[0]-margin.left); // TODO: CONSTRAIN WITHIN RIGHT MARGIN
+          const ym = yScale.invert(mouse[1]-margin.top);
+          const i1 = d3.bisectLeft(dates, xm, 1);
+          const i0 = i1 - 1;
+          const i = xm - dates[i0] > dates[i1] - xm ? i1 : i0;
+          const s = d3.least(state.data, d => Math.abs(d.values[i] - ym));
+          // console.log(s)
+          d3.selectAll(".curve")
+              .attr("opacity", 0.5)
+              .attr("stroke","lightgray")
+          d3.select(".curve."+nameNoSpaces(s[state.microzona]))
+            .attr("opacity", 1.0)
+            .attr("stroke", d => d3.interpolateViridis(d3.max(s.values, e => e)/yScale.domain()[1]))
+          path.attr("stroke", d => d === s ? null : "#ddd").filter(d => d === s).raise();
+          dot.attr("fill", d => d3.interpolateViridis(d3.max(s.values, e => e)/yScale.domain()[1]))
+            .attr("transform", `translate(${xScale(dates[i])+margin.left},${yScale(s.values[i])+margin.top})`);
+          dot.select("text").text(s.values[i]); // TODO: IF LOG THEN WE NEED TO SUBSTRACT ONE
+        }
+
+        function entered() {
+          path.style("mix-blend-mode", null).attr("stroke", "#ddd");
+          dot.attr("display", null);
+        }
+
+        function left() {
+          d3.selectAll(".curve")
+              .attr("opacity", 0.8)
+              .attr("stroke", d => d3.interpolateViridis(d3.max(d.values, e => e)/yScale.domain()[1]))
+          path.style("mix-blend-mode", "multiply").attr("stroke", null);
+          dot.attr("display", "none");
+        }
+      }
+
+
+      // state.data.forEach(function(ele){
+      //
+      //
+      //   texts.attr("x", width - margin.left/2 - margin.right/2)
+      //     .attr("y", yScale(datos[datos.length-1].y) + 5)
+      //     .attr("fill", d3.interpolateViridis(datos[datos.length-1].y/yScale.domain()[1]))
+      //     .attr("class", "name "+nameNoSpaces(ele[state.microzona]))
+      //     // .attr("opacity", 0.5)
+      //     .attr("text-anchor", "start")
+      //     .attr("font-size", "1rem")
+      //     // .attr("font-weight", "bold")
+      //     .on("mouseover", function(){
+      //       d3.selectAll(".curve")
+      //         .attr("opacity", 0.2)
+      //       d3.select(".curve."+nameNoSpaces(ele[state.microzona])).attr("opacity", 1.0)
+      //
+      //       d3.selectAll(".name")
+      //         .attr("opacity", 0.2)
+      //       d3.select(".name."+nameNoSpaces(ele[state.microzona])).attr("opacity", 1.0)
+      //     })
+      //     .on('mouseleave', function(){
+      //       d3.selectAll(".curve")
+      //         .attr("opacity", 1.0)
+      //
+      //       d3.selectAll(".name")
+      //         .attr("opacity", 1.0)
+      //     });
+      // })
+
+      // Update yAxis scale
+
+
+      if (state.escala === "escala-logaritmica"){
+        yScale = d3.scaleLog()
+            .range([height - margin.bottom, margin.top])
+            .domain([1, d3.max(state.data, d => d3.max(d.values)) + 1])
+        yAxis.scale(yScale)
+            .tickValues([2, 5, 10, 20, 50, 100, 200, 500, 1000, 2000])
+            // .ticks(2)
+            .tickFormat(d3.format('i'))
+        line.y(d => yScale(d + 1));
+      } else if (state.escala === "escala-lineal") {
+        yScale = d3.scaleLinear()
+            .range([height - margin.bottom, margin.top])
+            .domain([0, d3.max(state.data, d => d3.max(d.values))]).nice()
+        yAxis.scale(yScale);
+        line.y(d => yScale(d));
+      }
+
+      d3.select("g.axis.y")
+        .transition(200)
+        .duration(500)
+        .call(yAxis);
+
+      var selection = d3.selectAll(".curve").data(state.data)
+
+      selection.enter()
+        .append("path")
+        .transition(500)
+        .attr("d", function(d){
+          return line(d.values)
+        })
+
+      selection.transition(500)
+        .attr("d", function(d){
+          return line(d.values)
+        })
+
+      selection.exit().remove()
+
+
+      // texts.transition()
+      //   .duration(500)
+      //   .attr("y", 0)
+    }
   })
   .catch(function(error){
     console.log(error)
